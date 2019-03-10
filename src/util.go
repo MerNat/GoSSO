5a49e05b5e51bf8a5ecf6733f48375c3141a5fac
Meron <mernat777@gmail.com>
2019-03-10 13:56:42 +0300

    feat: Logger Added

--- src/util.go
+++ src/util.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"os"
 )
 
+//Configuration holds the struct to the config
+type Configuration struct {
+	Address string
+	Port    string
+}
+
 var outputLogger *log.Logger
+
+//Config holds the values used to start the server
+var Config Configuration
 
 func init() {
 	file, err := os.OpenFile("sso.log", os.O_CREATE|os.O_APPEND, 0666)
@@ -15,4 +25,35 @@ func init() {
 	}
 
 	outputLogger = log.New(file, "INFO ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func load() {
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Fatalln("Can not open config file", err)
+	}
+	err = json.NewDecoder(file).Decode(&Config)
+
+	if err != nil {
+		log.Fatalln("Can not decode config file", err)
+	}
+}
+
+func danger(args ...interface{}) {
+	outputLogger.SetPrefix("DANGER ")
+	outputLogger.Println(args...)
+}
+
+func warning(args ...interface{}) {
+	outputLogger.SetPrefix("WARNING ")
+	outputLogger.Println(args...)
+}
+
+func error(args ...interface{}) {
+	outputLogger.SetPrefix("ERROR ")
+	outputLogger.Println(args...)
+}
+
+func info(args ...interface{}) {
+	outputLogger.Println(args...)
 }
