6d6f26684b5bf3ade42ac846db3c0bd7b2b63d0d
Meron <mernat777@gmail.com>
2019-03-10 17:12:08 +0300

    feat:GeneralUtil handlers added

---
+++ src/handlers/util.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Message returns the message about to send to the client
+func Message(status bool, message string) map[string]interface{} {
+	return map[string]interface{}{"status": status, "message": message}
+}
+
+// Respond returns data to the client
+func Respond(w http.ResponseWriter, data map[string]interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
