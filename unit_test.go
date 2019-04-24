807668651fce040360912f9080136d09a890307d
Meron <mernat777@gmail.com>
2019-04-24 15:00:54 +0300

    fix #5 Final

--- unit_test.go
+++ unit_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -21,9 +22,14 @@ type Users struct {
 	Users []UserTest `json:"users"`
 }
 
+type Token struct {
+	Token string `json:"token"`
+}
+
 var mux2 *http.ServeMux
 var writer *httptest.ResponseRecorder
 var users Users
+var tokens []Token
 var apiURL string
 
 func TestMain(m *testing.M) {
@@ -48,7 +54,7 @@ func setUp() {
 	writer = httptest.NewRecorder()
 }
 
-func TestHandlePost(t *testing.T) {
+func TestHandleRegister(t *testing.T) {
 	endpoint := "/sso/register"
 	u, _ := url.ParseRequestURI(apiURL)
 	u.Path = endpoint
@@ -57,6 +63,47 @@ func TestHandlePost(t *testing.T) {
 		json, _ := json.Marshal(users.Users[i])
 		request, _ := http.Post(urlPath, "application/json", bytes.NewBuffer(json))
 		if request.StatusCode != 201 {
+			t.Errorf("Response code is %v\n", request.StatusCode)
+			break
+		}
+	}
+}
+
+func TestHandleLogin(t *testing.T) {
+	endpoint := "/sso/login"
+	u, _ := url.ParseRequestURI(apiURL)
+	u.Path = endpoint
+	urlPath := u.String()
+	for i := 0; i < len(users.Users); i++ {
+		jsonData, _ := json.Marshal(map[string]string{
+			"email":    users.Users[i].Email,
+			"password": users.Users[i].Password,
+		})
+		request, _ := http.Post(urlPath, "application/json", bytes.NewBuffer(jsonData))
+		body, _ := ioutil.ReadAll(request.Body)
+		token := Token{}
+		json.Unmarshal(body, &token)
+		tokens = append(tokens, token)
+		if request.StatusCode != 200 {
+			t.Errorf("Response code is %v\n", request.StatusCode)
+			break
+		}
+	}
+}
+
+func TestHandleVerify(t *testing.T) {
+	endpoint := "/sso/verify"
+	u, _ := url.ParseRequestURI(apiURL)
+	u.Path = endpoint
+	urlPath := u.String()
+	for _, v := range tokens {
+		jsonData, _ := json.Marshal(v)
+		request, _ := http.Post(urlPath, "application/json", bytes.NewBuffer(jsonData))
+		body, _ := ioutil.ReadAll(request.Body)
+		token := Token{}
+		json.Unmarshal(body, &token)
+		tokens = append(tokens, token)
+		if request.StatusCode != 200 {
 			t.Errorf("Response code is %v\n", request.StatusCode)
 			break
 		}
